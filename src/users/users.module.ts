0e73f3def2914e20a771ae0c0991cf1edbf71008
jonathanduran7 <joniduran624@gmail.com>
2024-04-06 20:00:45 -0300

    feat(users: implement blacklist service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants/auth';
+import { BlacklistService } from './services/blacklist.service';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    })
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, BlacklistService]
 })
 export class UsersModule {}
